f158969751c659844dc5b05115e95a13487ede8d
Ivan Filimonov <snake73w@gmail.com>
2019-02-16 02:34:22 +0700

    init

---
+++ lib/geo.rb
@@ -0,0 +1,6 @@
+require "geo/version"
+
+module Geo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/geo_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Geo do
+  it 'has a version number' do
+    expect(Geo::VERSION).not_to be nil
+  end
+end
