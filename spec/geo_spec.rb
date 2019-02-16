350862d11c395593f70ca03ce282ad4d440ff402
Ivan Filimonov <snake73w@gmail.com>
2019-02-16 20:24:28 +0700

    add fixture

--- spec/geo_spec.rb
+++ spec/geo_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Geo do
     it 'returns geo info' do
       class HttpTest
         def get(_url)
-          "{\"as\":\"AS1586 DoD Network Information Center\",\"city\":\"Sierra Vista\",\"country\":\"United States\",\"countryCode\":\"US\",\"isp\":\"DoD Network Information Center\",\"lat\":31.5552,\"lon\":-110.35,\"org\":\"USAISC\",\"query\":\"134.234.3.2\",\"region\":\"AZ\",\"regionName\":\"Arizona\",\"status\":\"success\",\"timezone\":\"America/Phoenix\",\"zip\":\"85613\"}"
+          File.open('spec/fixtures/files/geo.txt').read
         end
       end
-      expected = { :as=>"AS1586 DoD Network Information Center", :city=>"Sierra Vista", :country=>"United States", :countryCode=>"US", :isp=>"DoD Network Information Center", :lat=>31.5552, :lon=>-110.35, :org=>"USAISC", :query=>"134.234.3.2", :region=>"AZ", :regionName=>"Arizona", :status=>"success", :timezone=>"America/Phoenix", :zip=>"85613" }
+      expected = JSON.parse(File.open('spec/fixtures/files/geo.txt').read, symbolize_names: true)
       info = Geo::Geo.new(HttpTest.new).geo_info_by_ip('134.234.3.2')
       is_asserted_by { info == expected }
     end
